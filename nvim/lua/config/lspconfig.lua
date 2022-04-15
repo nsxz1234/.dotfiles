96ec1db756ac76720229eecc28d76e2ba45870d0
nsxz <alien_yip@qq.com>
2022-04-16 02:07:45 +0800

    chore: update

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -18,7 +18,6 @@ return function()
   end
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  -- capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
   local servers = { 'rust_analyzer', 'tsserver' }
