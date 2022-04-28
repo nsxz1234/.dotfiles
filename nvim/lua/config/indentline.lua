f77f9d64e5096f368ee6a4d8dfe9aa144136f1e3
nsxz <alien_yip@qq.com>
2022-04-28 23:05:43 +0800

    chore(indentline): Update

--- nvim/lua/config/indentline.lua
+++ nvim/lua/config/indentline.lua
@@ -1,8 +1,8 @@
 return function()
   require('indent_blankline').setup {
-    char = '┊', -- │ ┆ ┊ 
+    char = '', -- │ ┆ ┊ 
     show_foldtext = false,
-    context_char = '│',
+    context_char = '┊',
     show_current_context = true,
     show_current_context_start = false,
     show_current_context_start_on_current_line = false,
