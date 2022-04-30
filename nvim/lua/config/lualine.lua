fe571a7b5ee168cdd31a32f7a6f552ce22787ab1
nsxz <alien_yip@qq.com>
2022-04-30 17:15:58 +0800

    chore(lualine): Update

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,8 +1,21 @@
 return function()
+  local branch = {
+    'branch',
+    icon = { '' },
+  }
+  local file_name = {
+    'filename',
+    path = 1, -- 0: Just the filename  1: Relative path  2: Absolute path
+  }
+  local diff = {
+    'diff',
+    colored = true,
+    symbols = { added = ' ', modified = ' ', removed = ' ' },
+  }
   local function get_short_cwd()
     return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
   end
-  local neotree = {
+  local neo_tree = {
     sections = { lualine_a = { get_short_cwd } },
     filetypes = { 'neo-tree' },
   }
@@ -14,12 +27,12 @@ return function()
     },
     sections = {
       lualine_a = { 'mode' },
-      lualine_b = { 'branch' },
-      lualine_c = { 'filename' },
-      lualine_x = { 'diff' },
+      lualine_b = { branch },
+      lualine_c = { file_name },
+      lualine_x = { diff },
       lualine_y = { 'progress' },
       lualine_z = { 'location' },
     },
-    extensions = { neotree, 'symbols-outline', 'toggleterm' },
+    extensions = { neo_tree, 'symbols-outline', 'toggleterm' },
   }
 end
