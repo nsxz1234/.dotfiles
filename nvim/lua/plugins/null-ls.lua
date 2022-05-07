1b8fb8583b3f722176e8f0a9953ebd720df456b3
nsxz <alien_yip@qq.com>
2022-05-07 22:03:41 +0800

    feat(null-ls): setup on_attach

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,6 +1,7 @@
 return function()
   local null_ls = require('null-ls')
   null_ls.setup({
+    on_attach = as.lsp.on_attach,
     sources = {
       null_ls.builtins.diagnostics.zsh,
       null_ls.builtins.formatting.prettier.with({
