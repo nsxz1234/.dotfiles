a69f9f9a3ff951487a7e3992b56b06bc1a341920
nsxz <alien_yip@qq.com>
2022-05-07 22:01:00 +0800

    refactor(null-ls): remove builtins gitsigns, zigfmt, stylua

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -2,10 +2,7 @@ return function()
   local null_ls = require('null-ls')
   null_ls.setup({
     sources = {
-      null_ls.builtins.code_actions.gitsigns, -- dart formatting
       null_ls.builtins.diagnostics.zsh,
-      null_ls.builtins.formatting.zigfmt,
-      null_ls.builtins.formatting.stylua, -- install stylua
       null_ls.builtins.formatting.prettier.with({
         filetypes = { 'html', 'json', 'yaml', 'graphql', 'markdown' },
       }), -- install prettier
