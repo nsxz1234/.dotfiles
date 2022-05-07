3b4345863f14aa6f63dec9829a7f7bf56c3cb9da
nsxz <alien_yip@qq.com>
2022-05-07 22:39:24 +0800

    refactor(plugins): pull out flutter tools config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -128,34 +128,4 @@ return function()
     },
   })
   require('lspconfig').sumneko_lua.setup(luadev)
-
-  require('flutter-tools').setup({
-    ui = { border = 'rounded' },
-    debugger = {
-      enabled = true,
-      run_via_dap = true,
-    },
-    outline = { auto_open = false },
-    decorations = {
-      statusline = { device = true, app_version = true },
-    },
-    -- widget_guides = { enabled = true, debug = true },
-    dev_log = { enabled = false, open_cmd = 'tabedit' },
-    closing_tags = {
-      prefix = '>', -- character to use for close tag e.g. > Widget
-      enabled = true, -- set to false to disable
-    },
-    lsp = {
-      color = {
-        enabled = true,
-        background = true,
-        virtual_text = false,
-      },
-      settings = {
-        showTodos = true,
-        renameFilesWithClasses = 'prompt',
-      },
-      on_attach = as.lsp.on_attach,
-    },
-  })
 end
