89a4d7026adddcf70dde095ef67af658fa9f3093
nsxz <alien_yip@qq.com>
2022-05-11 15:50:28 +0800

    style(lsp.lua): use single quote

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -8,8 +8,8 @@ vim.diagnostic.config({
 --
 -- Signs
 --
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
+  local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
