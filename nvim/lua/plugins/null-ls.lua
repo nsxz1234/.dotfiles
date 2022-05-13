d431ada5fcc60c681a1501b711709f6314da46df
nsxz <alien_yip@qq.com>
2022-05-13 17:24:17 +0800

    feat(null-ls): add buf protobuf linter, set debounce

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,8 +1,10 @@
 return function()
   local null_ls = require('null-ls')
   null_ls.setup({
+    debounce = 150,
     on_attach = as.lsp.on_attach,
     sources = {
+      null_ls.builtins.diagnostics.buf,
       null_ls.builtins.diagnostics.zsh,
       null_ls.builtins.formatting.stylua, -- install stylua
       null_ls.builtins.formatting.prettier.with({
