d0a7779c898f634bbf1aa3383d19c7b14b488adf
nsxz <alien_yip@qq.com>
2022-05-13 17:26:08 +0800

    chore(plugins): update

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -3,16 +3,6 @@ as.safe_require('impatient')
 local utils = require('utils.plugins')
 local conf = utils.conf
 
----Some plugins are not safe to be reloaded because their setup functions
----and are not idempotent. This wraps the setup calls of such plugins
----@param func fun()
-function as.block_reload(func)
-  if vim.g.packer_compiled_loaded then
-    return
-  end
-  func()
-end
-
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -44,11 +34,7 @@ require('packer').startup(function(use)
   use({
     'lewis6991/satellite.nvim',
     config = function()
-      -- FIXME: this should be reported upstream at some point This plugin
-      -- is not safe to reload due to repeated attempts to map already mapped keys
-      as.block_reload(function()
-        require('satellite').setup()
-      end)
+      require('satellite').setup()
     end,
   })
   use({
