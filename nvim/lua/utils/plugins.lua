6a4f7a6d69a00fa8f0dfb55fc4f5b9bd8bbc2765
nsxz <alien_yip@qq.com>
2022-06-11 01:31:16 +0800

    chore(utils): update

--- nvim/lua/utils/plugins.lua
+++ nvim/lua/utils/plugins.lua
@@ -5,6 +5,16 @@ local fmt = string.format
 M.is_work = vim.env.WORK ~= nil
 M.is_home = not M.is_work
 
+---A thin wrapper around vim.notify to add packer details to the message
+---@param msg string
+function M.packer_notify(msg, level)
+  vim.notify(msg, level, { title = 'Packer' })
+end
+
+function M.not_headless()
+  return #vim.api.nvim_list_uis() > 0
+end
+
 ---Require a plugin config
 ---@param name string
 ---@return any
