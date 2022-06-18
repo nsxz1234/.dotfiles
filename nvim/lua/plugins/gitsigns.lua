abd76d338a5ed4672ae4d3197588501b123239a2
nsxz <alien_yip@qq.com>
2022-06-18 13:31:07 +0800

    chore(gitsigns): update

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -39,21 +39,18 @@ return function()
       require('which-key').register({
         ['<leader>h'] = {
           name = '+gitsigns hunk',
-          ['<space>'] = { gs.stage_hunk, 'stage' },
           u = { gs.undo_stage_hunk, 'undo stage' },
-          r = { gs.reset_hunk, 'reset hunk' },
           p = { gs.preview_hunk, 'preview current hunk' },
-          b = 'blame current line',
+          ['<space>'] = { gs.stage_hunk, 'stage' },
+          r = { gs.reset_hunk, 'reset hunk' },
+          b = { gs.blame_line, 'gitsigns: blame current line' },
         },
         ['<leader>g'] = {
           name = '+git',
           ['<space>'] = { gs.stage_buffer, 'gitsigns: stage entire buffer' },
           r = { gs.reset_buffer, 'gitsigns: reset entire buffer' },
-          b = { gs.blame_line, 'gitsigns: blame current line' },
           w = { gs.toggle_word_diff, 'gitsigns: toggle word diff' },
         },
-        ['[h'] = 'go to previous git hunk',
-        [']h'] = 'go to next git hunk',
         ['<leader>gq'] = { qf_list_modified, 'gitsigns: list modified in quickfix' },
       })
 
@@ -62,26 +59,24 @@ return function()
         vim.schedule(function()
           gs.prev_hunk()
         end)
-        return '<Ignore>'
-      end, { expr = true })
+        return '<Ignor>'
+      end, { expr = true, desc = 'go to previous git hunk' })
 
       as.nnoremap(']h', function()
         vim.schedule(function()
           gs.next_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true })
+      end, { expr = true, desc = 'go to next git hunk' })
 
-      vim.keymap.set('v', '<leader>h<space>', function()
+      as.vnoremap('<leader>h<space>', function()
         gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
       end)
-      vim.keymap.set('v', '<leader>hr', function()
+      as.vnoremap('<leader>hr', function()
         gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
       end)
 
       vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-      vim.keymap.set({ 'n' }, '<leader>h<space>', '<cmd>Gitsigns stage_hunk<CR>')
-      vim.keymap.set({ 'n' }, '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
     end,
   })
 end
