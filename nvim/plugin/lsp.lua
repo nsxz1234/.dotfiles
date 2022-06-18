73eea57e1e6bd1c6960f3bc075816bae0ae0f2a5
nsxz <alien_yip@qq.com>
2022-06-18 14:11:58 +0800

    fix(lsp): don't attach if a buffer is invalid

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,3 +1,5 @@
+local api = vim.api
+
 if vim.env.DEVELOPING then
   vim.lsp.set_log_level(vim.lsp.log_levels.DEBUG)
 end
@@ -87,6 +89,10 @@ as.augroup('LspSetupCommands', {
     desc = 'setup the language server autocommands',
     command = function(args)
       local bufnr = args.buf
+      -- if the buffer is invalid we should not try and attach to it
+      if not api.nvim_buf_is_valid(args.buf) then
+        return
+      end
       local client = vim.lsp.get_client_by_id(args.data.client_id)
       on_attach(client, bufnr)
     end,
