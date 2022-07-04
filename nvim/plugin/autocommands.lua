60b4afc0041d2fb1f6d483824a9bfbd94ab4de36
nsxz <alien_yip@qq.com>
2022-07-05 00:29:48 +0800

    feat(autocommands): add last edit position autocmd

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -1,3 +1,13 @@
+-- last edit position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = { '*' },
+  callback = function()
+    if vim.fn.line('\'"') > 1 and vim.fn.line('\'"') <= vim.fn.line('$') then
+      vim.api.nvim_exec('normal! g\'"', false)
+    end
+  end,
+})
+
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
