49c39911508f6076584aad328d8d33c646fd65dd
nsxz <alien_yip@qq.com>
2022-07-08 15:53:01 +0800

    update

--- nvim/filetype.lua
+++ nvim/filetype.lua
@@ -1,1 +1,18 @@
 if not vim.filetype then return end
+
+vim.filetype.add({
+  extension = {
+    lock = 'yaml',
+  },
+  filename = {
+    ['NEOGIT_COMMIT_EDITMSG'] = 'NeogitCommitMessage',
+    ['.gitignore'] = 'conf',
+    ['launch.json'] = 'jsonc',
+  },
+  pattern = {
+    ['.*%.conf'] = 'conf',
+    ['.*%.theme'] = 'conf',
+    ['.*%.gradle'] = 'groovy',
+    ['.*%.env%..*'] = 'env',
+  },
+})
