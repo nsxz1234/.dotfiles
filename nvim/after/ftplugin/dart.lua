1973348f0e74f8b231ba9d1023aeb4b25740b923
nsxz <alien_yip@qq.com>
2022-07-08 16:09:50 +0800

    refactor(ftplugin): migrate to as.ftplugin_conf helper function

--- nvim/after/ftplugin/dart.lua
+++ nvim/after/ftplugin/dart.lua
@@ -1,25 +1,25 @@
-local success, wk = pcall(require, 'which-key')
-if not success then
-  return
-end
-
-wk.register({
-  f = {
-    name = '+flutter',
-    b = {
-      "<cmd>TermExec cmd='flutter pub run build_runner build --delete-conflicting-outputs'<CR>",
-      'flutter: run code generation',
-    },
-    d = { '<Cmd>FlutterDevices<CR>', 'flutter: devices' },
-    e = { '<Cmd>FlutterEmulators<CR>', 'flutter: emulators' },
-    o = { '<Cmd>FlutterOutlineToggle<CR>', 'flutter: outline' },
-    q = { '<Cmd>FlutterQuit<CR>', 'flutter: quit' },
-    g = { '<Cmd>FlutterRun<CR>', 'run' },
-    r = { '<Cmd>FlutterRestart<CR>', 'restart' },
-    c = { '<Cmd>FlutterLogClear<CR>', 'log clear' },
-    p = { '<Cmd>FlutterPubGet<CR>', 'flutter: pub get' },
-    t = { '<Cmd>FlutterDevTools<CR>', 'flutter: dev tools' },
-  },
-}, {
-  prefix = '<localleader>',
-})
+as.ftplugin_conf(
+  'which-key',
+  function(wk)
+    wk.register({
+      f = {
+        name = '+flutter',
+        b = {
+          "<cmd>TermExec cmd='flutter pub run build_runner build --delete-conflicting-outputs'<CR>",
+          'flutter: run code generation',
+        },
+        d = { '<Cmd>FlutterDevices<CR>', 'flutter: devices' },
+        e = { '<Cmd>FlutterEmulators<CR>', 'flutter: emulators' },
+        o = { '<Cmd>FlutterOutlineToggle<CR>', 'flutter: outline' },
+        q = { '<Cmd>FlutterQuit<CR>', 'flutter: quit' },
+        g = { '<Cmd>FlutterRun<CR>', 'run' },
+        r = { '<Cmd>FlutterRestart<CR>', 'restart' },
+        c = { '<Cmd>FlutterLogClear<CR>', 'log clear' },
+        p = { '<Cmd>FlutterPubGet<CR>', 'flutter: pub get' },
+        t = { '<Cmd>FlutterDevTools<CR>', 'flutter: dev tools' },
+      },
+    }, {
+      prefix = '<localleader>',
+    })
+  end
+)
