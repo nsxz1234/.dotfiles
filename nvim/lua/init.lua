ded6f1e9b1d6ffe4f9250e3c59a15d295f7a63ed
nsxz <alien_yip@qq.com>
2022-07-10 14:48:05 +0800

    chore(neogit): update

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -432,11 +432,26 @@ require('packer').startup(function(use)
   use({ 'lewis6991/gitsigns.nvim', event = 'CursorHold', config = conf('gitsigns') })
   use({
     'TimUntersberger/neogit',
-    cmd = 'Neogit',
-    keys = { '<leader>gs' },
     requires = 'plenary.nvim',
-    setup = conf('neogit').setup,
-    config = conf('neogit').config,
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup({
+        disable_signs = false,
+        disable_hint = true,
+        disable_commit_confirmation = true,
+        disable_builtin_notifications = true,
+        disable_insert_on_commit = false,
+        signs = {
+          section = { '', '' }, -- "", ""
+          item = { '▸', '▾' },
+          hunk = { '樂', '' },
+        },
+        integrations = {
+          diffview = true,
+        },
+      })
+      as.nnoremap('<leader>gs', function() neogit.open() end, 'neogit: open status buffer')
+    end,
   })
   use({
     'sindrets/diffview.nvim',
