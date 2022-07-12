297a5764f5432437c03efdea50660b63024dcf59
nsxz <alien_yip@qq.com>
2022-07-12 17:46:25 +0800

    chore(null-ls): update

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -5,7 +5,10 @@ return function()
     sources = {
       null_ls.builtins.diagnostics.buf,
       null_ls.builtins.diagnostics.zsh,
-      null_ls.builtins.formatting.stylua, -- install stylua
+      -- install stylua
+      null_ls.builtins.formatting.stylua.with({
+        condition = function() return as.executable('stylua') end,
+      }),
       null_ls.builtins.formatting.prettier.with({
         filetypes = { 'html', 'json', 'yaml', 'graphql', 'markdown' },
       }), -- install prettier
