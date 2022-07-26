6a7a16ab8c1a62acfb1512791347e7ab8018d3c4
nsxz <alien_yip@qq.com>
2022-07-26 23:01:17 +0800

    chore(lspconfig): reduce number of workspace folders to speed up intialisation

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,4 +1,6 @@
 return function()
+  local fn, api = vim.fn, vim.api
+
   -- nvim-cmp supports additional completion capabilities
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
@@ -11,6 +13,11 @@ return function()
   end
 
   -- luadev
+  local lib = vim.tbl_filter(function(p)
+    if p:match('emmy') then return true end
+    return not vim.startswith(p, fn.stdpath('data') .. '/site/')
+  end, api.nvim_get_runtime_file('', true))
+
   require('lspconfig').sumneko_lua.setup({
     capabilities = capabilities,
     settings = {
@@ -19,9 +26,9 @@ return function()
           globals = { 'vim', 'describe', 'it', 'before_each', 'after_each', 'packer_plugins' },
         },
         completion = { keywordSnippet = 'Replace', callSnippet = 'Replace' },
-        -- workspace = {
-        --   library = vim.api.nvim_get_runtime_file('', true),
-        -- },
+        workspace = {
+          library = lib,
+        },
       },
     },
   })
