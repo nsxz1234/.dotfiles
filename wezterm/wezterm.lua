0b48b4e180eab9e590712b6c6104bdfc5b337f94
nsxz <alien_yip@qq.com>
2022-08-04 00:28:21 +0800

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+-- WezTerm
+-- https://wezfurlong.org/wezterm/
+
+local wezterm = require('wezterm')
+
+return {
+  -- Smart tab bar [distraction-free mode]
+  hide_tab_bar_if_only_one_tab = true,
+
+  window_background_opacity = 0.80,
+
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+
+  window_decorations = 'NONE',
+
+  font = wezterm.font('FantasqueSansMono Nerd Font'),
+
+  font_size = 17.0,
+
+  default_cursor_style = 'BlinkingBar',
+
+  line_height = 1.12,
+
+  keys = {
+    { key = 'i', mods = 'CTRL', action = wezterm.action.SendString('\x1b[105;5u') },
+  },
+}
