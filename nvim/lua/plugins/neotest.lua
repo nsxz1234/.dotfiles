f82587a634dea7beae13dc6d05c3e5b092cb4b22
nsxz <alien_yip@qq.com>
2022-08-23 23:21:46 +0800

    feat(neotest): add sync test file mapping

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -22,8 +22,13 @@ return function()
 
   local function run_file() neotest.run.run(vim.fn.expand('%')) end
 
+  local function run_file_sync()
+    require('neotest').run.run({ vim.fn.expand('%'), concurrent = false })
+  end
+
   as.nnoremap('<leader>ts', neotest.summary.toggle, 'neotest: run suite')
   as.nnoremap('<leader>to', open, 'neotest: output')
   as.nnoremap('<leader>tn', neotest.run.run, 'neotest: run')
   as.nnoremap('<leader>tf', run_file, 'neotest: run file')
+  as.nnoremap('<localleader>tF', run_file_sync, 'neotest: run file synchronously')
 end
