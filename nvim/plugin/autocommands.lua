9f6cb5bb061d132b0fd4bc077107256f572f00da
nsxz <alien_yip@qq.com>
2022-08-31 22:23:44 +0800

    refactor(spell): set via one autocommand

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -11,9 +11,31 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
   group = highlight_group,
   pattern = '*',
+})
+
+as.augroup('Utilities', {
+  {
+    event = { 'FileType' },
+    pattern = {
+      'lua',
+      'vim',
+      'dart',
+      'python',
+      'javascript',
+      'typescript',
+      'rust',
+      'org',
+      'NeogitCommitMessage',
+      'go',
+      'markdown',
+      'zig',
+    },
+    -- FIXME: spellsitter is slow in large files
+    -- TODO: should this be done in ftplugin files
+    -- NOTE: setting spell only works using opt_local otherwise it leaks into subsequent windows
+    command = function(args) vim.opt_local.spell = vim.api.nvim_buf_line_count(args.buf) < 8000 end,
+  },
 })
