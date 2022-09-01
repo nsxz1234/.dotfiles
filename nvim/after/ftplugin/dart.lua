4879be46740a7f8527ac1f814030450cbbe90ac2
nsxz <alien_yip@qq.com>
2022-09-01 20:22:30 +0800

    refactor(dart): remove which key dependency

--- nvim/after/ftplugin/dart.lua
+++ nvim/after/ftplugin/dart.lua
@@ -1,28 +1,20 @@
 vim.bo.syntax = ''
 
 if not as then return end
-as.ftplugin_conf(
-  'which-key',
-  function(wk)
-    wk.register({
-      f = {
-        name = '+flutter',
-        b = {
-          "<cmd>TermExec cmd='flutter pub run build_runner build --delete-conflicting-outputs'<CR>",
-          'flutter: run code generation',
-        },
-        d = { '<Cmd>FlutterDevices<CR>', 'flutter: devices' },
-        e = { '<Cmd>FlutterEmulators<CR>', 'flutter: emulators' },
-        o = { '<Cmd>FlutterOutlineToggle<CR>', 'flutter: outline' },
-        q = { '<Cmd>FlutterQuit<CR>', 'flutter: quit' },
-        g = { '<Cmd>FlutterRun<CR>', 'run' },
-        r = { '<Cmd>FlutterRestart<CR>', 'restart' },
-        c = { '<Cmd>FlutterLogClear<CR>', 'log clear' },
-        p = { '<Cmd>FlutterPubGet<CR>', 'flutter: pub get' },
-        t = { '<Cmd>FlutterDevTools<CR>', 'flutter: dev tools' },
-      },
-    }, {
-      prefix = '<localleader>',
-    })
-  end
+
+local function with_desc(desc) return { desc = desc } end
+
+as.nnoremap(
+  '<localleader>fb',
+  "<cmd>TermExec cmd='flutter pub run build_runner build --delete-conflicting-outputs'<CR>",
+  'flutter: run code generation'
 )
+as.nnoremap('<localleader>fd', '<Cmd>FlutterDevices<CR>', with_desc('flutter: devices'))
+as.nnoremap('<localleader>fe', '<Cmd>FlutterEmulators<CR>', with_desc('flutter: emulators'))
+as.nnoremap('<localleader>fo', '<Cmd>FlutterOutlineToggle<CR>', with_desc('flutter: outline'))
+as.nnoremap('<localleader>fq', '<Cmd>FlutterQuit<CR>', with_desc('flutter: quit'))
+as.nnoremap('<localleader>fg', '<Cmd>FlutterRun<CR>', with_desc('flutter: server run'))
+as.nnoremap('<localleader>fr', '<Cmd>FlutterRestart<CR>', with_desc('flutter: server restart'))
+as.nnoremap('<localleader>fc', '<Cmd>FlutterLogClear<CR>', with_desc('flutter: log clear'))
+as.nnoremap('<localleader>fp', '<Cmd>FlutterPubGet<CR>', with_desc('flutter: pub get'))
+as.nnoremap('<localleader>ft', '<Cmd>FlutterDevTools<CR>', with_desc('flutter: dev tools'))
