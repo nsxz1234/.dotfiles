4aa0b8ee9a7a6b129bf4473b30c9eb23e0f0f54c
nsxz <alien_yip@qq.com>
2022-10-17 09:24:54 +0800

    refactor(cmp)!: update lsp setup method

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return function()
 
   -- nvim-cmp supports additional completion capabilities
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
   local servers = { 'zls' }
   for _, lsp in ipairs(servers) do
