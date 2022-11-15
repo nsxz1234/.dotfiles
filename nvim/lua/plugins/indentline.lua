4e14430b6a1e622da630ada9ef2f538863b611cd
nsxz <alien_yip@qq.com>
2022-11-15 18:42:41 +0800

    chore(indentline): update

--- nvim/lua/plugins/indentline.lua
+++ nvim/lua/plugins/indentline.lua
@@ -1,13 +1,12 @@
 return function()
   require('indent_blankline').setup({
-    char = '', -- │ ┆ ┊ 
+    char = '┊', -- │ ┆ ┊ 
     show_foldtext = false,
-    context_char = '┊',
-    show_current_context = true,
+    show_current_context = false,
     show_current_context_start = false,
     show_current_context_start_on_current_line = false,
     show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
+    show_first_indent_level = false,
     filetype_exclude = {
       'neo-tree-popup',
       'dap-repl',
