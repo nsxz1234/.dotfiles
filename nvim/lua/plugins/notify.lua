17f9643acaa2e9c9f549df15e3311f2cffe6377c
nsxz <alien_yip@qq.com>
2022-12-23 16:50:15 +0800

    chore(notify): update

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -2,6 +2,8 @@ return function()
   local notify = require('notify')
   notify.setup({
     timeout = 3000,
+    render = 'minimal',
+    max_width = function() return math.floor(vim.o.columns * 0.4) end,
   })
   vim.notify = notify
   require('telescope').load_extension('notify')
