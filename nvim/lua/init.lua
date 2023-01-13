aaeb22bfcaa51311cc0b6cf6f945bea173782326
nsxz <alien_yip@qq.com>
2023-01-13 19:07:07 +0800

    update

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -22,6 +22,7 @@ require('lazy').setup(
       priority = 1000,
       config = function()
         vim.g.gruvbox_material_background = 'soft'
+        -- vim.g.gruvbox_material_transparent_background = 2
         vim.cmd.colorscheme('gruvbox-material')
       end,
     },
