bf5acef99df50c8e055c7e596814647699bf3970
nsxz <alien_yip@qq.com>
2023-02-10 09:23:27 +0800

    chore(cmp): update sources order

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -55,8 +55,8 @@ return function()
       end,
     },
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
       { name = 'luasnip' },
+      { name = 'nvim_lsp' },
       { name = 'path' },
       {
         name = 'rg',
