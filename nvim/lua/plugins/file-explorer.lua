1d892aed2eb159a1881a3b35e9daff53212d0c67
nsxz <alien_yip@qq.com>
2023-02-16 09:42:34 +0800

    style(explorer): re-arrange config

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -1,73 +1,68 @@
-local function config()
-  vim.g.neo_tree_remove_legacy_commands = 1
-  local icons = as.style.icons
-  as.nnoremap('<tab>', '<Cmd>Neotree toggle reveal<CR>')
-  as.nnoremap('<C-e>', '<Cmd>Neotree toggle reveal<CR>')
-  require('neo-tree').setup({
-    sources = {
-      'filesystem',
-      'buffers',
-      'git_status',
-      'diagnostics',
-    },
-    enable_git_status = true,
-    git_status_async = true,
-    nesting_rules = {
-      ['dart'] = { 'freezed.dart', 'g.dart' },
-    },
-    event_handlers = {
-      {
-        event = 'neo_tree_buffer_enter',
-        handler = function() vim.cmd('highlight! Cursor blend=100') end,
-      },
-      {
-        event = 'neo_tree_buffer_leave',
-        handler = function() vim.cmd('highlight! Cursor blend=0') end,
-      },
-    },
-    filesystem = {
-      hijack_netrw_behavior = 'open_current',
-      use_libuv_file_watcher = true,
-      group_empty_dirs = true,
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-    },
-    default_component_configs = {
-      indent = {
-        padding = 0,
-      },
-      git_status = {
-        symbols = {
-          added = icons.git.add,
-          deleted = icons.git.remove,
-          modified = icons.git.mod,
-          renamed = icons.git.rename,
-          untracked = '',
-          ignored = '',
-          unstaged = '',
-          staged = '',
-          conflict = '',
-        },
-      },
-    },
-    window = {
-      width = 30,
-      mappings = {
-        ['o'] = 'toggle_node',
-      },
-    },
-  })
-end
+local icons = as.style.icons
 
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v2.x',
-    config = config,
-    keys = { '<tab>', '<C-e>' },
+    keys = {
+      { '<tab>', '<Cmd>Neotree toggle reveal<CR>', desc = 'NeoTree' },
+    },
+    config = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+
+      require('neo-tree').setup({
+        sources = { 'filesystem', 'buffers', 'git_status', 'diagnostics' },
+        enable_git_status = true,
+        git_status_async = true,
+        nesting_rules = {
+          ['dart'] = { 'freezed.dart', 'g.dart' },
+        },
+        event_handlers = {
+          {
+            event = 'neo_tree_buffer_enter',
+            handler = function() vim.cmd('highlight! Cursor blend=100') end,
+          },
+          {
+            event = 'neo_tree_buffer_leave',
+            handler = function() vim.cmd('highlight! Cursor blend=0') end,
+          },
+        },
+        filesystem = {
+          hijack_netrw_behavior = 'open_current',
+          use_libuv_file_watcher = true,
+          group_empty_dirs = true,
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+          },
+        },
+        default_component_configs = {
+          indent = {
+            padding = 0,
+          },
+          git_status = {
+            symbols = {
+              added = icons.git.add,
+              deleted = icons.git.remove,
+              modified = icons.git.mod,
+              renamed = icons.git.rename,
+              untracked = '',
+              ignored = '',
+              unstaged = '',
+              staged = '',
+              conflict = '',
+            },
+          },
+        },
+        window = {
+          width = 30,
+          mappings = {
+            ['o'] = 'toggle_node',
+          },
+        },
+      })
+    end,
     dependencies = {
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
