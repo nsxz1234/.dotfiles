ce2db2ee2eff3960253487df37709e4dcf0d1f4b
nsxz <alien_yip@qq.com>
2023-02-20 10:05:04 +0800

    chore(completion): make copilot accept silent

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -128,7 +128,11 @@ return {
     dependencies = { 'nvim-cmp' },
     init = function() vim.g.copilot_no_tab_map = true end,
     config = function()
-      map('i', '<Plug>(as-copilot-accept)', "copilot#Accept('<Tab>')", { expr = true })
+      map('i', '<Plug>(as-copilot-accept)', "copilot#Accept('<Tab>')", {
+        expr = true,
+        remap = true,
+        silent = true,
+      })
       map('i', '<M-]>', '<Plug>(copilot-next)')
       map('i', '<M-[>', '<Plug>(copilot-previous)')
       map('i', '<C-\\>', '<Cmd>vertical Copilot panel<CR>')
