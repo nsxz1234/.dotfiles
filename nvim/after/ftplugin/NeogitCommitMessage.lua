6f5f84ec7d523df9a8ed7851a17578ab42fe8c19
nsxz <alien_yip@qq.com>
2023-02-26 14:22:16 +0800

    chore(neogit): update

--- nvim/after/ftplugin/NeogitCommitMessage.lua
+++ nvim/after/ftplugin/NeogitCommitMessage.lua
@@ -6,10 +6,6 @@ opt.colorcolumn = '50,72'
 
 if not as then return end
 as.ftplugin_conf({
-  ['nvim-treesitter.parsers'] = function(parsers)
-    -- make sure neogit commits use the treesitter parser
-    parsers.filetype_to_parsername['NeogitCommitMessage'] = 'gitcommit'
-  end,
   cmp = function(cmp)
     cmp.setup.filetype('NeogitCommitMessage', {
       sources = cmp.config.sources({
