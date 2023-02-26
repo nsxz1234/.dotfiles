75890a6b44d544089c20b4d58e5b647e928da42f
nsxz <alien_yip@qq.com>
2023-02-26 14:40:07 +0800

    chore(neogit): check register function

--- nvim/after/ftplugin/NeogitCommitMessage.lua
+++ nvim/after/ftplugin/NeogitCommitMessage.lua
@@ -4,6 +4,10 @@ opt.list = false
 opt.spelllang = 'en_gb'
 opt.colorcolumn = '50,72'
 
+if vim.treesitter.language.register then
+  vim.treesitter.language.register('gitcommit', 'NeogitCommitMessage')
+end
+
 if not as then return end
 as.ftplugin_conf({
   cmp = function(cmp)
