4b611a203e8274d18da7615156d10cd9f2aac393
nsxz <alien_yip@qq.com>
2023-02-27 09:57:27 +0800

    refactor(tools): let mason-null-ls do the driving

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -1,29 +1,20 @@
 return {
   {
-    'jay-babu/mason-null-ls.nvim',
-    dependencies = { 'mason.nvim' },
-    opts = { automatic_installation = true },
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
   },
   {
-    'jose-elias-alvarez/null-ls.nvim',
+    'jay-babu/mason-null-ls.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = { 'nvim-lua/plenary.nvim', 'mason-null-ls.nvim' },
+    dependencies = { 'mason.nvim', 'null-ls.nvim' },
     config = function()
-      local null_ls = require('null-ls')
-      null_ls.setup({
-        debounce = 150,
-        sources = {
-          null_ls.builtins.diagnostics.buf,
-          null_ls.builtins.diagnostics.zsh,
-          -- install stylua
-          null_ls.builtins.formatting.stylua.with({
-            condition = function() return as.executable('stylua') end,
-          }),
-          null_ls.builtins.formatting.prettier.with({
-            filetypes = { 'html', 'json', 'yaml', 'graphql', 'markdown' },
-          }), -- install prettier
-        },
+      require('mason-null-ls').setup({
+        automatic_setup = true,
+        automatic_installation = {},
+        ensure_installed = { 'buf', 'stylua', 'prettier' },
       })
+      require('null-ls').setup()
+      require('mason-null-ls').setup_handlers()
     end,
   },
 }
