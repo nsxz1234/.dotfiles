13b376c69ea5d71f8cd6f319f4cbef9d1044f0d3
nsxz <alien_yip@qq.com>
2023-02-28 09:16:33 +0800

    style(completion): cosmetic re-org

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,107 +1,106 @@
-local function config()
-  local cmp = require('cmp')
-  local api, fn = vim.api, vim.fn
-  local t = as.replace_termcodes
-  local luasnip = require('luasnip')
-
-  local function tab(fallback)
-    if luasnip.expand_or_locally_jumpable() then
-      luasnip.expand_or_jump()
-    elseif cmp.visible() then
-      cmp.select_next_item()
-    else
-      fallback()
-    end
-  end
-
-  local function shift_tab(fallback)
-    if luasnip.jumpable(-1) then
-      luasnip.jump(-1)
-    elseif cmp.visible() then
-      cmp.select_prev_item()
-    else
-      fallback()
-    end
-  end
-
-  cmp.setup({
-    matching = {
-      disallow_partial_fuzzy_matching = false,
-    },
-    snippet = {
-      expand = function(args) require('luasnip').lsp_expand(args.body) end,
-    },
-    mapping = {
-      ['<C-h>'] = cmp.mapping(
-        function(_) api.nvim_feedkeys(fn['copilot#Accept'](t('<Tab>')), 'n', true) end
-      ),
-      ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-      ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-      ['<Tab>'] = cmp.mapping(tab, { 's', 'c' }),
-      ['<C-n>'] = cmp.mapping(tab, { 'i', 's' }),
-      ['<C-p>'] = cmp.mapping(shift_tab, { 'i', 's' }),
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<CR>'] = cmp.mapping.confirm({ select = false }),
-      ['jj'] = cmp.mapping.confirm({ select = true }),
-      ['<C-q>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-    },
-    formatting = {
-      deprecated = true,
-      fields = { 'kind', 'abbr', 'menu' },
-      format = function(entry, vim_item)
-        vim_item.kind = as.ui.lsp.kinds[vim_item.kind]
-        vim_item.dup = ({
-          luasnip = 0,
-        })[entry.source.name] or 0
-        return vim_item
-      end,
-    },
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-      { name = 'path' },
-      {
-        name = 'rg',
-        keyword_length = 4,
-        max_item_count = 10,
-        option = { additional_arguments = '--max-depth 8' },
-      },
-    }, {
-      {
-        name = 'buffer',
-        options = {
-          get_bufnrs = function() return vim.api.nvim_list_bufs() end,
-        },
-      },
-      { name = 'spell' },
-    }),
-  })
-
-  local search_sources = {
-    sources = cmp.config.sources(
-      { { name = 'nvim_lsp_document_symbol' } },
-      { { name = 'buffer' } }
-    ),
-  }
-
-  cmp.setup.cmdline('/', search_sources)
-  cmp.setup.cmdline('?', search_sources)
-  cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({
-      { name = 'cmdline', keyword_pattern = [=[[^[:blank:]\!]*]=] },
-      { name = 'path' },
-      { name = 'cmdline_history', priority = 10, max_item_count = 5 },
-    }),
-  })
-end
+local api, fn = vim.api, vim.fn
+local t = as.replace_termcodes
 
 return {
   {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
-    config = config,
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      local function tab(fallback)
+        if luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        elseif cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end
+
+      local function shift_tab(fallback)
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        elseif cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end
+
+      cmp.setup({
+        matching = {
+          disallow_partial_fuzzy_matching = false,
+        },
+        snippet = {
+          expand = function(args) require('luasnip').lsp_expand(args.body) end,
+        },
+        mapping = {
+          ['<C-h>'] = cmp.mapping(
+            function(_) api.nvim_feedkeys(fn['copilot#Accept'](t('<Tab>')), 'n', true) end
+          ),
+          ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+          ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+          ['<Tab>'] = cmp.mapping(tab, { 's', 'c' }),
+          ['<C-n>'] = cmp.mapping(tab, { 'i', 's' }),
+          ['<C-p>'] = cmp.mapping(shift_tab, { 'i', 's' }),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['jj'] = cmp.mapping.confirm({ select = true }),
+          ['<C-q>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        },
+        formatting = {
+          deprecated = true,
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, vim_item)
+            vim_item.kind = as.ui.lsp.kinds[vim_item.kind]
+            vim_item.dup = ({
+              luasnip = 0,
+            })[entry.source.name] or 0
+            return vim_item
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+          {
+            name = 'rg',
+            keyword_length = 4,
+            max_item_count = 10,
+            option = { additional_arguments = '--max-depth 8' },
+          },
+        }, {
+          {
+            name = 'buffer',
+            options = {
+              get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+            },
+          },
+          { name = 'spell' },
+        }),
+      })
+
+      local search_sources = {
+        sources = cmp.config.sources(
+          { { name = 'nvim_lsp_document_symbol' } },
+          { { name = 'buffer' } }
+        ),
+      }
+
+      cmp.setup.cmdline('/', search_sources)
+      cmp.setup.cmdline('?', search_sources)
+      cmp.setup.cmdline(':', {
+        sources = cmp.config.sources({
+          { name = 'cmdline', keyword_pattern = [=[[^[:blank:]\!]*]=] },
+          { name = 'path' },
+          { name = 'cmdline_history', priority = 10, max_item_count = 5 },
+        }),
+      })
+    end,
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-cmdline' },
