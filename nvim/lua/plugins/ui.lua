d5152d073ee14c058b973aebdfd066954223a680
nsxz <alien_yip@qq.com>
2023-03-01 09:31:18 +0800

    refactor(plugins): add bufferline to ui section

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,3 +1,6 @@
+local fn = vim.fn
+local icons = as.ui.icons.lsp
+
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
@@ -55,6 +58,74 @@ return {
         function() notify.dismiss({ silent = true, pending = true }) end,
         { desc = 'dismiss notifications' }
       )
+    end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local groups = require('bufferline.groups')
+      require('bufferline').setup({
+        options = {
+          debug = { logging = true },
+          mode = 'buffers', -- tabs
+          sort_by = 'insert_after_current',
+          right_mouse_command = 'vert sbuffer %d',
+          show_close_icon = false,
+          diagnostics = 'nvim_lsp',
+          diagnostics_indicator = function(count, level)
+            level = level:match('warn') and 'warn' or level
+            return (icons[level] or '?') .. ' ' .. count
+          end,
+          diagnostics_update_in_insert = false,
+          offsets = {
+            {
+              text = ' DIFF VIEW',
+              filetype = 'DiffviewFiles',
+              highlight = 'PanelHeading',
+            },
+            {
+              text = ' FLUTTER OUTLINE',
+              filetype = 'flutterToolsOutline',
+              highlight = 'PanelHeading',
+            },
+          },
+          groups = {
+            options = { toggle_hidden_on_enter = true },
+            items = {
+              groups.builtin.pinned:with({ icon = '' }),
+              groups.builtin.ungrouped,
+              {
+                name = 'Terraform',
+                matcher = function(buf) return buf.name:match('%.tf') ~= nil end,
+              },
+              {
+                name = 'tests',
+                icon = '',
+                matcher = function(buf)
+                  return buf.filename:match('_spec') or buf.filename:match('_test')
+                end,
+              },
+              {
+                name = 'docs',
+                icon = '',
+                matcher = function(buf)
+                  for _, ext in ipairs({ 'md', 'txt', 'org', 'norg', 'wiki' }) do
+                    if ext == fn.fnamemodify(buf.path, ':e') then return true end
+                  end
+                end,
+              },
+            },
+          },
+        },
+      })
+
+      map('n', 'd<space>', ':BufferLinePickClose<cr>')
+      map('n', 'H', ':BufferLineCyclePrev<cr>')
+      map('n', 'L', ':BufferLineCycleNext<cr>')
+      map('n', '<m-H>', ':BufferLineMovePrev<CR>')
+      map('n', '<m-L>', ':BufferLineMoveNext<CR>')
     end,
   },
 }
