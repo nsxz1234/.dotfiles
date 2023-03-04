cfcd50f6db969e623baa89152f045f1eca74c0e8
nsxz <alien_yip@qq.com>
2023-03-04 13:10:51 +0800

    fix(treesitter): disable syntax entirely

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -18,6 +18,7 @@ return {
         auto_install = true,
         highlight = {
           enable = true,
+          additional_vim_regex_highlighting = false,
         },
         incremental_selection = {
           enable = true,
