926baf4164928bb60b3c10bc7f942dba197b73e8
nsxz <alien_yip@qq.com>
2023-03-05 13:01:17 +0800

    chore(dashboard): remove pokemon.nvim

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -4,13 +4,7 @@ return {
   config = function()
     local alpha = require('alpha')
     local dashboard = require('alpha.themes.dashboard')
-    local pokemon = require('pokemon')
-    pokemon.setup({
-      number = 'random',
-      size = 'tiny',
-    })
-    dashboard.section.header.val = pokemon.header()
     alpha.setup(dashboard.config)
   end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons', 'ColaMint/pokemon.nvim' } },
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
 }
