405acfb9254296b907ebaf9442f2dd91a801a719
nsxz <alien_yip@qq.com>
2023-04-08 14:22:39 +0800

    chore(treesitter): disable highlight

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -20,7 +20,7 @@ return {
         },
         auto_install = true,
         highlight = {
-          enable = true,
+          enable = false,
           additional_vim_regex_highlighting = false,
         },
         incremental_selection = {
