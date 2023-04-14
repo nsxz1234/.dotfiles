913ae6cf6cbf86dfee3bc67be05f64cf9c3569eb
nsxz <alien_yip@qq.com>
2023-04-14 12:09:04 +0800

    fix(queries): readd dart indents

---
+++ nvim/queries/dart/indents.scm
@@ -0,0 +1,11 @@
+; extends
+[
+  (for_statement)
+  (while_statement)
+  (do_statement)
+  (if_element)
+  (for_element)
+] @indent.begin
+
+(conditional_expression) @indent.auto
+(if_null_expression) @indent.auto
