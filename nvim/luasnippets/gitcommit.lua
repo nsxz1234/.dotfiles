c7253ea3ac9db9483578cef37f26a219699b3c4a
nsxz <alien_yip@qq.com>
2023-04-26 09:45:57 +0800

    chore(snippets): update gitcommit snippet

--- nvim/luasnippets/gitcommit.lua
+++ nvim/luasnippets/gitcommit.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable: undefined-global
 return {
   snippet({ trig = 'll', name = 'Lazy lockfile' }, {
-    t('chore(lazy): update lockfile'),
+    t('chore: update lockfile'),
   }),
 }
