79a32c557c05acfec8de34b5a742f27465fe1f3a
nsxz <alien_yip@qq.com>
2023-06-19 17:12:58 +0800

    chore(plugins): add dropbar.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,22 @@
+local ui = as.ui
 local icons = as.ui.icons.lsp
+
+local lspkind = require('lspkind')
+
 return {
+  {
+    'Bekaboo/dropbar.nvim',
+    event = 'VeryLazy',
+    keys = { { '<leader>w', function() require('dropbar.api').pick() end, desc = 'winbar: pick' } },
+    config = {
+      icons = {
+        ui = { bar = { separator = ' ' .. ui.icons.misc.arrow_right .. ' ' } },
+        kinds = {
+          symbols = vim.tbl_map(function(value) return value .. ' ' end, lspkind.symbol_map),
+        },
+      },
+    },
+  },
   {
     'lukas-reineke/virt-column.nvim',
     event = 'VimEnter',
