5e96ec365da0ab8795b40333d6748e24db08fbce
nsxz <alien_yip@qq.com>
2023-06-25 16:36:14 +0800

    chore(flash): update

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -203,7 +203,12 @@ return {
   },
   {
     'folke/flash.nvim',
-    opts = {},
+    event = 'VeryLazy',
+    opts = {
+      modes = {
+        char = { enabled = false },
+      },
+    },
     keys = {
       { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end },
       { 'S', mode = { 'n', 'o', 'x' }, function() require('flash').treesitter() end },
