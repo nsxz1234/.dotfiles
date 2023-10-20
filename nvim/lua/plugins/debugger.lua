6f66dc6a2b0b77f030fbe94e3c8c3530487051e3
nsxz <alien_yip@qq.com>
2023-10-20 12:16:28 +0800

    chore(dap): add zig config

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -18,5 +18,32 @@ return {
       },
       { 'theHamsta/nvim-dap-virtual-text', opts = { all_frames = true } },
     },
+    config = function()
+      local dap = require('dap')
+
+      dap.adapters.codelldb = {
+        type = 'server',
+        port = '${port}',
+        executable = {
+          command = vim.fn.stdpath('data') .. '/mason/bin/codelldb',
+          args = { '--port', '${port}' },
+        },
+      }
+
+      local configurations = {
+        {
+          name = 'Launch file',
+          type = 'codelldb',
+          request = 'launch',
+          cwd = '${workspaceFolder}',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+        },
+      }
+
+      dap.configurations.c = configurations
+      dap.configurations.zig = configurations
+    end,
   },
 }
