798e280e0d897f219f756ace83ed058aff917be4
nsxz <alien_yip@qq.com>
2023-10-29 00:34:52 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ return {
   -- Smart tab bar [distraction-free mode]
   hide_tab_bar_if_only_one_tab = true,
 
-  window_background_opacity = 0.80,
+  window_background_opacity = 0.85,
 
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
 
@@ -23,5 +23,30 @@ return {
 
   keys = {
     { key = 'i', mods = 'CTRL', action = wezterm.action.SendString('\x1b[105;5u') },
+  },
+
+  color_scheme = 'gruvbox_material_dark_hard',
+  color_schemes = {
+    ['gruvbox_material_dark_hard'] = {
+      background = '#32302f',
+      foreground = '#D4BE98',
+      cursor_bg = '#D4BE98',
+      cursor_border = '#D4BE98',
+      cursor_fg = '#1D2021',
+      selection_bg = '#D4BE98',
+      selection_fg = '#3C3836',
+
+      ansi = { '#1d2021', '#ea6962', '#a9b665', '#d8a657', '#7daea3', '#d3869b', '#89b482', '#d4be98' },
+      brights = {
+        '#eddeb5',
+        '#ea6962',
+        '#a9b665',
+        '#d8a657',
+        '#7daea3',
+        '#d3869b',
+        '#89b482',
+        '#d4be98',
+      },
+    },
   },
 }
