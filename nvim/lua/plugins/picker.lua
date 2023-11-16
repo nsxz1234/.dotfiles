0373aa520bfb1075aa443d3721e8302b65a36c24
nsxz <alien_yip@qq.com>
2023-11-16 21:13:28 +0800

    .

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -16,17 +16,17 @@ return {
     },
     keys = {
       { 'ff', file_picker, desc = 'find files' },
-      { 'fg', fzf_lua.live_grep, desc = 'live grep' },
       { 'fo', fzf_lua.oldfiles, desc = 'Most recently used files' },
+      { 'fd', fzf_lua.buffers, desc = 'buffers' },
+      { 'fg', fzf_lua.live_grep, desc = 'live grep' },
       { 'f;', fzf_lua.commands, desc = 'commands' },
       { 'fc', fzf_lua.command_history, desc = 'command history' },
       { 'fh', fzf_lua.git_bcommits, desc = 'buffer commits' },
-      { 'fd', fzf_lua.buffers, desc = 'buffers' },
       { 'f/', fzf_lua.help_tags, desc = 'help' },
       { 'ft', fzf_lua.lsp_live_workspace_symbols, desc = 'workspace symbols' },
       { 'fa', fzf_lua.lsp_document_symbols, desc = 'document symbols' },
-      { 'fk', fzf_lua.keymaps, desc = 'keymaps' },
       { 'fb', fzf_lua.grep_curbuf, desc = 'current buffer fuzzy find' },
+      { 'fk', fzf_lua.keymaps, desc = 'keymaps' },
     },
     config = function()
       local lsp_kind = require('lspkind')
