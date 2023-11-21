9c375b21014dbb13cc93bf561de4ca7ba9b2af27
nsxz <alien_yip@qq.com>
2023-11-21 15:27:27 +0800

    .

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -37,10 +37,10 @@ return {
     config = function()
       map('n', '<leader>hh', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>')
       map('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<CR>')
-      map('n', '<m-1>', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>')
-      map('n', '<m-2>', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>')
-      map('n', '<m-3>', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>')
-      map('n', '<m-4>', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>')
+      map('n', '<m-q>', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>')
+      map('n', '<m-w>', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>')
+      map('n', '<m-e>', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>')
+      map('n', '<m-r>', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>')
     end,
   },
   {
