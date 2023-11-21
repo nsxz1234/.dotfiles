32fdc6cba6f3986624c483b103459a11ad9f1d48
nsxz <alien_yip@qq.com>
2023-11-21 19:42:37 +0800

    .

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ for file in $dotfiles/config/*; do
   ln -sf $file ~/.config/
 done
 
+sudo mkdir -p /usr/local/share/wayland-sessions/
 sudo cp wayland-sessions/river.desktop /usr/local/share/wayland-sessions/
