a126715a4b4775886f17fbc57741c458c6cd73d5
nsxz <alien_yip@qq.com>
2024-01-26 16:28:32 +0800

    .

--- config/nvim/lua/plugins/themes.lua
+++ config/nvim/lua/plugins/themes.lua
@@ -1,16 +1,30 @@
+local function highlights()
+  vim.api.nvim_set_hl(0, 'DiagnosticUnnecessary', { link = 'Comment' })
+  vim.api.nvim_set_hl(0, 'BqfPreviewBorder', { link = 'Normal' })
+  -- https://github.com/neovim/neovim/issues/26037#issuecomment-1838548013
+  -- if this is merged into master we can delete.
+  vim.api.nvim_set_hl(0, 'WinBar', { link = 'NONE' })
+end
+
 return {
   {
     'sainnhe/gruvbox-material',
     priority = 1000,
     config = function()
       vim.g.gruvbox_material_background = 'soft'
-      -- vim.g.gruvbox_material_transparent_background = 2
+
       vim.cmd.colorscheme('gruvbox-material')
-      vim.api.nvim_set_hl(0, 'DiagnosticUnnecessary', { link = 'Comment' })
-      vim.api.nvim_set_hl(0, 'BqfPreviewBorder', { link = 'Normal' })
-      -- https://github.com/neovim/neovim/issues/26037#issuecomment-1838548013
-      -- if this is merged into master we can delete.
-      vim.api.nvim_set_hl(0, 'WinBar', { link = 'NONE' })
+      highlights()
+    end,
+  },
+  {
+    'sainnhe/everforest',
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = 'soft'
+
+      -- vim.cmd.colorscheme('everforest')
+      -- highlights()
     end,
   },
 }
