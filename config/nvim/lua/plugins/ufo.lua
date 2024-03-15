cd4d5b5bf2dbc17574545d06f90d6dc13e2c742a
nsxz <alien_yip@qq.com>
2024-03-15 14:40:47 +0800

    .

--- config/nvim/lua/plugins/ufo.lua
+++ config/nvim/lua/plugins/ufo.lua
@@ -14,7 +14,7 @@ return {
       open_fold_hl_timeout = 0,
       preview = { win_config = { winhighlight = 'Normal:Normal,FloatBorder:Normal' } },
       enable_get_fold_virt_text = true,
-      close_fold_kinds = { 'imports', 'comment' },
+      close_fold_kinds_for_ft = { 'imports', 'comment' },
       provider_selector = function(_, ft) return ft_map[ft] or { 'treesitter', 'indent' } end,
     })
   end,
