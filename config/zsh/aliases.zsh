120971e86449532ecb5ef00949d27b88a658bb24
nsxz <alien_yip@qq.com>
2024-03-27 15:57:32 -0700

    .

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias ssh="env TERM=xterm-256color ssh"
 alias ls="ls --color=auto --hyperlink=auto $@"
 alias l="eza --long --all --git --color=always --group-directories-first --icons $@"
 alias lt="eza --icons --all --color=always -T $@"
-alias e='./nvim-macos-arm64/bin/nvim'
+alias e='~/nvim-macos-arm64/bin/nvim'
 alias lg='lazygit'
 alias ra='ranger'
 alias grep='grep --color'
