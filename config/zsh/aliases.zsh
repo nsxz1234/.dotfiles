29a3c37ec616903cefe0d6c1fe813e00549d5829
nsxz <alien_yip@qq.com>
2024-03-28 17:26:46 +0800

    .

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias l="eza --long --all --git --color=always --group-directories-first --icons
 alias lt="eza --icons --all --color=always -T $@"
 alias e='~/nvim-macos-arm64/bin/nvim'
 alias lg='lazygit'
-alias ra='ranger'
+alias y='yazi'
 alias grep='grep --color'
 alias re='wf-recorder -g "$(slurp)" -f recording.mp4'
 alias t='ydcv-rs'
