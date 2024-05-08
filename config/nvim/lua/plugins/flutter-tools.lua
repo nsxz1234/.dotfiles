b827c977804a0c50eebc8ea1431b5dcad7730666
nsxz <alien_yip@qq.com>
2024-05-08 15:22:03 +0800

    .

--- config/nvim/lua/plugins/flutter-tools.lua
+++ config/nvim/lua/plugins/flutter-tools.lua
@@ -29,7 +29,7 @@ return {
         analysisExcludedFolders = {
           vim.fn.expand('$HOME/flutter'),
           vim.fn.expand('$HOME/.pub-cache'),
-          vim.fn.expand('$HOME/workspace/app-remotecontrol-flutter/build'),
+          vim.fn.expand('$HOME/workspace/app-wifi-flutter/build'),
         },
         showTodos = false,
         renameFilesWithClasses = 'always',
