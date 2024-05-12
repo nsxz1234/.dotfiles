873c08a3fdb57ebac07f1123def32ce61b81e3f9
nsxz <alien_yip@qq.com>
2024-05-12 18:34:42 +0800

    .

--- config/nvim/lua/plugins/debugger.lua
+++ config/nvim/lua/plugins/debugger.lua
@@ -3,12 +3,12 @@ return {
     'mfussenegger/nvim-dap',
     keys = {
       { '<leader>b', '<cmd>lua require("dap").toggle_breakpoint()<cr>' },
-      { '<leader>1', '<cmd>lua require("dap").continue()<cr>' },
-      { '<leader>2', '<cmd>lua require("dap").step_into()<cr>' },
-      { '<leader>3', '<cmd>lua require("dap").step_over()<cr>' },
-      { '<leader>4', '<cmd>lua require("dap").step_out()<cr>' },
-      { '<leader>5', '<cmd>lua require("dap").repl.toggle()<cr>' },
-      { '<leader>6', '<cmd>lua require("dap").run_last()<cr>' },
+      { '<c-1>', '<cmd>lua require("dap").continue()<cr>' },
+      { '<c-2>', '<cmd>lua require("dap").step_into()<cr>' },
+      { '<c-3>', '<cmd>lua require("dap").step_over()<cr>' },
+      { '<c-4>', '<cmd>lua require("dap").step_out()<cr>' },
+      { '<c-5>', '<cmd>lua require("dap").repl.toggle()<cr>' },
+      { '<c-6>', '<cmd>lua require("dap").run_last()<cr>' },
       { '<leader>d', '<cmd>lua require("dapui").toggle()<cr>' },
     },
     dependencies = {
