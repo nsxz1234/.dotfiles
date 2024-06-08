39ebbed37cfb7ec241a2e684dc20aa7e87ce2369
nsxz <alien_yip@qq.com>
2024-06-09 00:27:19 +0800

    chore(plugins): add nvim-lint

--- config/nvim/lua/plugins/tools.lua
+++ config/nvim/lua/plugins/tools.lua
@@ -17,4 +17,18 @@ return {
       require('null-ls').setup()
     end,
   },
+  {
+    'mfussenegger/nvim-lint',
+    event = 'BufReadPre',
+    init = function()
+      vim.api.nvim_create_autocmd({ 'TextChanged' }, {
+        callback = function() require('lint').try_lint() end,
+      })
+    end,
+    config = function()
+      require('lint').linters_by_ft = {
+        markdown = { 'markdownlint' },
+      }
+    end,
+  },
 }
