855861a7d4ee85caea6ba4680ee3b4918602d891
nsxz <alien_yip@qq.com>
2024-06-23 22:30:25 -0300

    chore(gitsigns): deprecated

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -66,7 +66,6 @@ return {
         untracked = { text = '░' },
       },
       _inline2 = false,
-      _signs_staged_enable = false,
       on_attach = function()
         local gs = package.loaded.gitsigns
 
