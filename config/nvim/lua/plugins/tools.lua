4bd6a50854f06e3085195b0fad117e3eb3bbb8c3
nsxz <alien_yip@qq.com>
2024-07-27 17:26:51 +0800

    refactor(tools): replace none-ls with conform.nvim

--- config/nvim/lua/plugins/tools.lua
+++ config/nvim/lua/plugins/tools.lua
@@ -1,21 +1,14 @@
 return {
   {
-    'nvimtools/none-ls.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
-  {
-    'jay-babu/mason-null-ls.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = { 'mason.nvim', 'none-ls.nvim' },
-    config = function()
-      require('mason-null-ls').setup({
-        automatic_setup = true,
-        automatic_installation = true,
-        ensure_installed = { 'buf', 'stylua', 'prettier' },
-        handlers = {},
-      })
-      require('null-ls').setup()
-    end,
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        zig = { 'zigfmt' },
+        lua = { 'stylua' },
+        markdown = { 'prettier' },
+        dart = { 'dart_format' },
+      },
+    },
   },
   {
     'mfussenegger/nvim-lint',
