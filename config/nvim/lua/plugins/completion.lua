5cb3b2e0666d797083d610540a25df18261f246e
nsxz <alien_yip@qq.com>
2024-09-04 12:37:44 +0800

    chore(plugins): add neocodeium

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -103,4 +103,17 @@ return {
       })
     end,
   },
+  {
+    'monkoose/neocodeium',
+    event = 'VeryLazy',
+    config = function()
+      local neocodeium = require('neocodeium')
+      neocodeium.setup()
+      vim.keymap.set('i', '<c-i>', neocodeium.accept)
+      vim.keymap.set('i', '<c-.>', neocodeium.accept_word)
+      vim.keymap.set('i', '<c-/>', neocodeium.accept_line)
+      vim.keymap.set('i', '<c-]>', function() neocodeium.cycle_or_complete() end)
+      vim.keymap.set('n', '<s-space>', '<cmd>NeoCodeium toggle<cr>')
+    end,
+  },
 }
