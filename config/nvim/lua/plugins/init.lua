a1593aa358ec50b45cf0df7e816fdfb1af96a8bc
nsxz <alien_yip@qq.com>
2024-09-06 21:21:56 +0800

    .

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -57,7 +57,6 @@ return {
       map('n', '-', '<cmd>Oil<CR>')
       require('oil').setup({
         keymaps = {
-          ['<C-s>'] = false,
           ['<C-h>'] = false,
           ['<C-l>'] = false,
           ['<C-t>'] = false,
