ee636fc3cca74d3e3b5ef8080ab308584c80fdf5
nsxz <alien_yip@qq.com>
2024-10-08 14:14:21 +0800

    .

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,3 +1,4 @@
+alias ..='cd ..'
 alias ssh="env TERM=xterm-256color ssh"
 alias ls="ls --color=auto --hyperlink=auto $@"
 alias l="eza --long --all --git --color=always --group-directories-first --icons $@"
@@ -8,4 +9,7 @@ alias y='yazi'
 alias grep='grep --color'
 alias re='wf-recorder -g "$(slurp)" -f recording.mp4'
 alias t='ydcv'
-
+alias err='journalctl -b -p err'
+alias cal='cal -3'
+alias ipv4="ip addr show | grep 'inet ' | grep -v '127.0.0.1' | cut -d' ' -f6 | cut -d/ -f1"
+alias ipv6="ip addr show | grep 'inet6 ' | cut -d ' ' -f6 | sed -n '2p'"
